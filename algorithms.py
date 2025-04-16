9f5a411a3c5400c1ed106d02c69146d68010b53b
MatheussBrant <matheus.s.brant@gmail.com>
2025-04-16 11:47:53 -0300

    Implementa função BFS para as recomendações de matérias por semestre
    Co-authored-by: caiolamego caio.lamego04@gmail.com

--- algorithms.py
+++ algorithms.py
@@ -11,3 +11,26 @@ class Grafo:
             self.grafo[sigla] = materia['pre_requisitos'] or []
             self.semestres[sigla] = materia['semestre_recomendado']
             self.nomes[sigla] = materia['nome']
+
+
+    def bfs_recomendacao(self, cursadas, semestre_atual, max_por_semestre=5):
+        recomendacoes = {}
+        semestre = semestre_atual
+        concluidas = set(cursadas)
+
+        while True: 
+            disponiveis = [
+                materia for materia in self.grafo
+                if materia not in concluidas and 
+                all(req in concluidas for req in self.grafo[materia])
+            ]
+
+            if not disponiveis:
+                break 
+
+            disponiveis.sort(key=lambda x: self.semestres[x])
+            recomendacoes_semestre = disponiveis[:max_por_semestre]
+            recomendacoes[f"Semestre {semestre}"] = recomendacoes_semestre
+            concluidas.update(recomendacoes_semestre)
+            semestre += 1 
+        return recomendacoes
